6de2ef287015877a11269d6f0deb6ed1fd8bab31
Shreyas-dev13 <spatil@intelligentdx.com>
2025-11-24 21:53:58 -0700

    Endpoint_Correction

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const BACKEND_BASE_URL = "https://backend.raml.elhlwgy.com/";
-
+const BACKEND_BASE_URL = "https://backend.raml.elhlwgy.com";
+// const localhostBaseURL = "http://localhost:8000";
 const API = {
   UPLOAD: `${BACKEND_BASE_URL}/api/analysis/upload`,
   REPORTS: `${BACKEND_BASE_URL}/api/analysis/reports`,
