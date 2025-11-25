e885def315505c87f9ee2583daac7baa1c343a64
Shreyas-dev13 <spatil@intelligentdx.com>
2025-11-25 09:04:49 -0700

    Updated Endpoints

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const LOCALHOST_BASE_URL = "http://localhost:8000/";
 //const BASE_URL = LOCALHOST_BASE_URL; 
 const BASE_URL = BACKEND_BASE_URL;
 
-export const API = {
+const API = {
   UPLOAD: `${BASE_URL}api/analysis/upload`,
   REPORTS: `${BASE_URL}api/analysis/reports`,
   DOWNLOAD_REPORT: `${BASE_URL}api/analysis/report`, // <-- added
